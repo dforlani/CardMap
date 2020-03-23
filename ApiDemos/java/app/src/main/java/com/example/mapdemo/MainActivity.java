f4a34e8d8a575136362baa31d6ee3a92b23d2fb7
dforlani <dforlani@gmail.com>
2020-03-23 17:23:14 -0300

    Trocada tela inicial para a do mapa e adicionado botão de adicionar localizações

--- ApiDemos/java/app/src/main/java/com/example/mapdemo/MainActivity.java
+++ ApiDemos/java/app/src/main/java/com/example/mapdemo/MainActivity.java
@@ -16,79 +16,329 @@
 
 package com.example.mapdemo;
 
-import android.content.Context;
+import android.Manifest;
 import android.content.Intent;
-import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+
+import com.example.mapdemo.OnMapAndViewReadyListener;
+import com.example.mapdemo.R;
+import com.example.mapdemo.activity.NovaLocalizacaoComFotoActivity;
+import com.example.mapdemo.database.DatabaseHelperLocalizacao;
+import com.example.mapdemo.model.Localizacao;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowCloseListener;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
- * The main activity of the API library demo gallery.
- * <p>
- * The main layout lists the demonstrated features, with buttons to launch them.
+ * This shows how to place markers on a map.
  */
-public final class MainActivity extends AppCompatActivity
-        implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements
+        OnMarkerClickListener,
+        OnInfoWindowClickListener,
+        OnMarkerDragListener,
+        OnSeekBarChangeListener,
+        OnInfoWindowLongClickListener,
+        OnInfoWindowCloseListener,
+        OnMapAndViewReadyListener.OnGlobalLayoutAndMapReadyListener {
 
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
 
-    /**
-     * A custom array adapter that shows a {@link FeatureView} containing details about the demo.
-     */
-    private static class CustomArrayAdapter extends ArrayAdapter<DemoDetails> {
+    private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
 
-        /**
-         * @param demos An array containing the details of the demos to be displayed.
-         */
-        public CustomArrayAdapter(Context context, DemoDetails[] demos) {
-            super(context, R.layout.feature, R.id.title, demos);
+    private static final LatLng DARWIN = new LatLng(-12.4634, 130.8456);
+
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
+
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+
+    private static final LatLng ALICE_SPRINGS = new LatLng(-24.6980, 133.8807);
+
+    private GoogleMap mMap;
+
+    private final List<Marker> mMarkerRainbow = new ArrayList<>();
+
+    private TextView mTopText;
+
+    List<Localizacao> localizacoes;
+
+    /** Demonstrates customizing the info window and/or its contents. */
+    class CustomInfoWindowAdapter implements InfoWindowAdapter {
+
+        // These are both viewgroups containing an ImageView with id "badge" and two TextViews with id
+        // "title" and "snippet".
+        private final View mWindow;
+
+        private final View mContents;
+
+        CustomInfoWindowAdapter() {
+            mWindow = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+            mContents = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            FeatureView featureView;
-            if (convertView instanceof FeatureView) {
-                featureView = (FeatureView) convertView;
+        public View getInfoWindow(Marker marker) {
+            //TODO
+            return null;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            //TODO
+            return null;
+        }
+
+        private void render(Marker marker, View view) {
+
+            ((ImageView) view.findViewById(R.id.badge)).setImageResource(0);
+
+            String title = marker.getTitle();
+            TextView titleUi = ((TextView) view.findViewById(R.id.title));
+            if (title != null) {
+                // Spannable string allows us to edit the formatting of the text.
+                SpannableString titleText = new SpannableString(title);
+                titleText.setSpan(new ForegroundColorSpan(Color.RED), 0, titleText.length(), 0);
+                titleUi.setText(titleText);
             } else {
-                featureView = new FeatureView(getContext());
+                titleUi.setText("");
             }
 
-            DemoDetails demo = getItem(position);
-
-            featureView.setTitleId(demo.titleId);
-            featureView.setDescriptionId(demo.descriptionId);
-
-            Resources resources = getContext().getResources();
-            String title = resources.getString(demo.titleId);
-            String description = resources.getString(demo.descriptionId);
-            featureView.setContentDescription(title + ". " + description);
-
-            return featureView;
+            String snippet = marker.getSnippet();
+            TextView snippetUi = ((TextView) view.findViewById(R.id.snippet));
+            if (snippet != null && snippet.length() > 12) {
+                SpannableString snippetText = new SpannableString(snippet);
+                snippetText.setSpan(new ForegroundColorSpan(Color.MAGENTA), 0, 10, 0);
+                snippetText.setSpan(new ForegroundColorSpan(Color.BLUE), 12, snippet.length(), 0);
+                snippetUi.setText(snippetText);
+            } else {
+                snippetUi.setText("");
+            }
         }
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        ListView list = (ListView) findViewById(R.id.list);
+        setContentView(R.layout.activity_mapa_das_localizacoejs);
 
-        ListAdapter adapter = new CustomArrayAdapter(this, DemoDetailsList.DEMOS);
-
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(this);
-        list.setEmptyView(findViewById(R.id.empty));
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        new OnMapAndViewReadyListener(mapFragment, this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        DemoDetails demo = (DemoDetails) parent.getAdapter().getItem(position);
-        startActivity(new Intent(this, demo.activityClass));
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+
+        // Hide the zoom controls as the button panel will cover it.
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+
+        // Insere as localizações no mapa
+        addMarkersToMap();
+
+        // Setting an info window adapter allows us to change the both the contents and look of the
+        // info window.
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+
+        // Set listeners for marker events.  See the bottom of this class for their behavior.
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+        mMap.setOnInfoWindowCloseListener(this);
+        mMap.setOnInfoWindowLongClickListener(this);
+
+        // Override the default content description on the view, for accessibility mode.
+        // Ideally this string would be localised.
+        mMap.setContentDescription("Map with lots of markers.");
+
+
+        LatLngBounds.Builder builderBounds = new LatLngBounds.Builder();
+
+        for (int i = 0; i < localizacoes.size(); i++) {
+            builderBounds.include(new LatLng(new Double(localizacoes.get(i).latitude), new Double(localizacoes.get(i).longitude)));
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(builderBounds.build(), 50));
     }
+
+    private void addMarkersToMap() {
+        //alterações para adicionar marcadores do banco
+        localizacoes  = this.getLocalizacoes();
+
+        for (int i = 0; i < localizacoes.size(); i++) {
+
+
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(new Double(localizacoes.get(i).latitude), new Double(localizacoes.get(i).longitude)))
+                    .icon(BitmapDescriptorFactory.fromBitmap(localizacoes.get(i).foto))
+                    .title(localizacoes.get(i).toString()));
+        }
+
+    }
+
+    public List<Localizacao> getLocalizacoes(){
+        DatabaseHelperLocalizacao db  = new DatabaseHelperLocalizacao(this);
+        ArrayList<Localizacao>  localizacoes = db.getAll();
+
+        return localizacoes;
+
+    }
+
+
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (!checkReady()) {
+            return;
+        }
+        float rotation = seekBar.getProgress();
+        for (Marker marker : mMarkerRainbow) {
+            marker.setRotation(rotation);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // Do nothing.
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // Do nothing.
+    }
+
+
+    // Método para clicar na imagem (ponto) do mapa
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Markers have a z-index that is settable and gettable.
+        float zIndex = marker.getZIndex() + 1.0f;
+        marker.setZIndex(zIndex);
+        Toast.makeText(this, marker.getTitle() + " z-index set to " + zIndex, Toast.LENGTH_SHORT).show();
+
+        onClickedImage(marker.getTitle().replaceAll("[^0-9]", "")); // chama método responsável para realizar ligação
+
+
+        // We return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(this, "Click Info Window", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onInfoWindowClose(Marker marker) {
+        //Toast.makeText(this, "Close Info Window", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onInfoWindowLongClick(Marker marker) {
+        Toast.makeText(this, "Info Window long click", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        mTopText.setText("onMarkerDragStart");
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        mTopText.setText("onMarkerDragEnd");
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        mTopText.setText("onMarkerDrag.  Current Position: " + marker.getPosition());
+    }
+
+
+    // Método que realiza ligações quando a imagem for clicada
+    public void onClickedImage(String telefone) {
+
+        Uri uri = Uri.parse("tel: " + telefone);
+
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+            return;
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * Ao retornar de uma chamada para adicionar uma localização, vai limpar o mapa e readicionar os marcadores
+     */
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(mMap != null) {
+            mMap.clear();
+            addMarkersToMap();
+        }
+
+    }
+
+    /**
+     * Chamado ao clicar no botão de adicionar localização
+     * */
+    public void onAdicionar(View view) {
+        startActivity(new Intent(this, NovaLocalizacaoComFotoActivity.class));;
+    }
+
 }
